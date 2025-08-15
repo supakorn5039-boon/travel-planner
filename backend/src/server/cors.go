4143ceddef7ab4121cb3d41de8b3eb60fc678506
Nebu1a <130897105+supakorn5039-boon@users.noreply.github.com>
2025-08-15 22:25:44 +0700

    feat: allow frontend path

--- backend/src/server/cors.go
+++ backend/src/server/cors.go
@@ -9,7 +9,7 @@ import (
 
 func applyCorsMiddleware(router *gin.Engine) {
 	c := cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://supakorn-travel.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
